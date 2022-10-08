4145b89a52b8ce097ec3a74561543b466860cd0f
Caio Silva <caio_ceac23@hotmail.com>
2022-10-08 14:32:07 -0300

    finished

--- gui/crud.py
+++ gui/crud.py
@@ -1,13 +1,23 @@
 from kivy.app import App
+from kivy.properties import partial
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 from kivy.uix.togglebutton import ToggleButton
 
 from entidades.cliente import Cliente
 from repositorios.cliente_repositorio import ClienteRepositorio
 
 
+class ExclusaoPopup(Popup):
+    pass
+
+
+class MensagemPopup(Popup):
+    pass
+
+
 class BotaoListagem(ToggleButton):
-    def __int__(self, cliente_id, cliente_nome, cliente_idade, **kwargs):
+    def __init__(self, cliente_id, cliente_nome, cliente_idade, **kwargs):
         super(BotaoListagem, self).__init__(**kwargs)
         self.id_cliente = cliente_id
         self.nome_cliente = cliente_nome
@@ -15,24 +25,63 @@ class BotaoListagem(ToggleButton):
         self.text = self.nome_cliente + " " + self.idade_cliente
         self.group = 'clientes'
 
+    def _do_release(self, *args):
+        Principal().cliente_selecionado(self.id_cliente)
+
 
 class Principal(BoxLayout):
+    id_cliente = 0
+
+    def __init__(self, **kwargs):
+        super(Principal, self).__init__(**kwargs)
+        self.listar_clientes()
+
+    def cliente_selecionado(self, id):
+        Principal.id_cliente = id
+
+    def editar_cliente(self):
+        id = Principal.id_cliente
+        nome = self.ids.nome.text
+        idade = self.ids.idade.text
+        if nome == '' or idade == '':
+            MensagemPopup().open()
+        else:
+            cliente = Cliente(nome, idade)
+            ClienteRepositorio.editar_cliente(id, cliente)
+            self.ids.nome.text = ''
+            self.ids.idade.text = ''
+            self.listar_clientes()
+
     def cadastrar_cliente(self):
         nome = self.ids.nome.text
         idade = self.ids.idade.text
-
-        cliente = Cliente(nome, idade)
-        ClienteRepositorio.inserir_cliente(cliente)
-        self.ids.nome.text = ''
-        self.ids.idade.text = ''
+        if nome == '' or idade == '':
+            MensagemPopup().open()
+        else:
+            cliente = Cliente(nome, idade)
+            ClienteRepositorio.inserir_cliente(cliente)
+            self.ids.nome.text = ''
+            self.ids.idade.text = ''
+            self.listar_clientes()
 
     def listar_clientes(self):
+        self.ids.clientes.clear_widgets()
         clientes = ClienteRepositorio.listar_cliente()
         for i in clientes:
             id = str(i[0])
             nome = i[1]
-            idade = i[2]
-            self.ids.cliente.add_widget(BotaoListagem(id, nome, idade))
+            idade = str(i[2])
+            self.ids.clientes.add_widget(BotaoListagem(id, nome, idade))
+
+    def remover_cliente(self):
+        id = Principal.id_cliente
+        popup = ExclusaoPopup()
+        popup.funcao = partial(self.remover, id)
+        popup.open()
+
+    def remover(self, id):
+        ClienteRepositorio.remover_cliente(id)
+        self.listar_clientes()
 
 
 class Crud(App):
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from repositorios import cliente_repositorio
-from entidades import cliente
-
-cliente = cliente.Cliente("Silva", 44)
-
-cliente_repositorio.ClienteRepositorio.inserir_cliente(cliente)
-
-
-cliente_repositorio.ClienteRepositorio.listar_cliente()
-
-# cliente_repositorio.ClienteRepositorio.editar_cliente(1, cliente)
-# cliente_repositorio.ClienteRepositorio.remover_cliente(8)
+# from repositorios import cliente_repositorio
+# from entidades import cliente
+#
+# cliente = cliente.Cliente("Silva", 44)
+#
+# cliente_repositorio.ClienteRepositorio.inserir_cliente(cliente)
+#
+#
+# cliente_repositorio.ClienteRepositorio.listar_cliente()
+#
+# # cliente_repositorio.ClienteRepositorio.editar_cliente(1, cliente)
+# # cliente_repositorio.ClienteRepositorio.remover_cliente(8)
 
 
 
